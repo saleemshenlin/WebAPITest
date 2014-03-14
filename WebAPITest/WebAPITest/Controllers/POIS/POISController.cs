aa36db1eae9dc22068b1c1f27ded71d84f0f70f5
沈霖 <saleemshenlin@gmail.com>
2014-03-14 16:44:37 +0800

    add  view index

---
+++ WebAPITest/WebAPITest/Controllers/POIS/POISController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPITest.Controllers.POIS
+{
+    public class POISController : Controller
+    {
+        //
+        // GET: /POI/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
