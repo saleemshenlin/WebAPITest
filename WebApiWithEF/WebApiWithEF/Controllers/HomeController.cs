e181f671b3739a05e5615a5bb6e349ea65526901
沈霖 <saleemshenlin@gmail.com>
2014-03-19 17:46:48 +0800

    WebAPIWithEF
    图片上传 完成一半

--- WebApiWithEF/WebApiWithEF/Controllers/HomeController.cs
+++ WebApiWithEF/WebApiWithEF/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -51,10 +55,81 @@ namespace WebApiWithEF.Controllers
             //绑定API的Url
             string apiUri = Url.HttpRouteUrl("DefaultApi", new { controller = "POI", });
             ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
-           
+
             string PoiId = Request.QueryString["id"];
             ViewBag.PoiId = PoiId;
             return View();
+        }
+        /// <summary>
+        /// 上传图片
+        /// </summary>
+        /// <param name="upImg"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult Upload(HttpPostedFileBase upImg)
+        {
+            string fileName = System.IO.Path.GetFileName(upImg.FileName.Substring(0, upImg.FileName.Length-4));
+            string newName = fileName + "_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".jpg";
+            string newMiniName = fileName + "_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + "_mini.jpg";
+            string filePhysicalPath = Server.MapPath("~/Update/" + newName);
+            string fileMiniPath = Server.MapPath("~/Update/" + newMiniName);
+            string pic = "", error = "";
+            Image oImg = Image.FromStream(upImg.InputStream);
+            Bitmap nImg = new Bitmap(720, 540);
+            Graphics graphics = Graphics.FromImage(nImg);
+            graphics.Clear(Color.White);
+            graphics.InterpolationMode = InterpolationMode.High;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.DrawImage(oImg, new Rectangle(0, 0, 720, 540));
+            graphics.Dispose();
+            oImg.Dispose();
+            Image miniImg = ResetImageSize(oImg, 200, 150);
+            try
+            {
+                nImg.Save(filePhysicalPath);
+                nImg.Dispose();
+                miniImg.Save(fileMiniPath);
+                miniImg.Dispose();
+                //upImg.SaveAs(filePhysicalPath);
+                pic = "/Update/" + fileMiniPath;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+            return Json(new
+            {
+                pic = pic,
+                error = error
+            });
+        }
+
+        /// <summary>
+        /// 一个空回调方法,ResetImageSize()方法调用
+        /// </summary>
+        /// <returns></returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static bool ImageCallback()
+        {
+            return false;
+        }
+
+
+        /// <summary>
+        /// 返回此 Image 对象的缩略图  
+        /// </summary>
+        /// <param name="AtImage">要缩放的图象对象</param>
+        /// <param name="ImageWidth">指定新的图象宽度</param>
+        /// <param name="ImageHeight">指定新的图象高度</param>
+        /// <returns></returns>
+        public static Image ResetImageSize(Image AtImage, int ImageWidth, int ImageHeight)
+        {
+            if (AtImage == null) return null;
+            Image.GetThumbnailImageAbort MyCallback = new Image.GetThumbnailImageAbort(ImageCallback);
+            Image Img = null;
+            Img = AtImage.GetThumbnailImage(ImageWidth, ImageHeight, MyCallback, IntPtr.Zero);
+            return Img;
         }
 
         /// <summary>
